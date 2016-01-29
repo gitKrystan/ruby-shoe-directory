926330230c8d091de1013d5bc259c73d8c7906e7
Krystan Menne <kmenne@gmail.com>
2016-01-29 10:24:56 -0800

    add validation for presence of store name

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -7,4 +7,9 @@ describe(Store) do
     brand2 = create_test_brand(store)
     expect(store.brands()).to(eq([brand1, brand2]))
   end
+
+  it('validates presence of name') do
+    test_store = Store.create(name: '')
+    expect(test_store.save).to(eq(false))
+  end
 end

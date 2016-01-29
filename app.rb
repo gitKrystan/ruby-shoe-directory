3be265c4084e2dd8514a36a74015f788da62e7ca
Krystan Menne <kmenne@gmail.com>
2016-01-29 11:04:33 -0800

    add initial paths

--- app.rb
+++ app.rb
@@ -1,1 +1,20 @@
-require "sinatra/activerecord"
+require('bundler/setup')
+require "pry"
+Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  erb(:index)
+end
+
+get '/admin' do
+  erb(:admin)
+end
+
+get '/stores/:id' do
+  erb(:store)
+end
+
+get '/stores/:id/edit' do
+  erb(:admin_store)
+end
